e32bb6eeede686dd39f33cbe6ed65c9a594dadba
Nihat4ik <aliyevnihats@outlook.com>
2022-01-19 20:40:48 +0400

    Major Bug Fixes. Redux Removal --> Context Added.

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import { Provider } from "react-redux";
-import { createStore, compose } from "redux";
-import reducers from "./reducers/reducers";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-ReactDOM.render(
-  <Provider store={createStore(reducers, composeEnhancers())}>
-    <App />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
--- src/reducers/reducers.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import activeReducer from "./activeReducer";
-import openedReducer from "../reducers/openedReducer";
-
-export default combineReducers({
-  active: activeReducer,
-  menu: openedReducer,
-});
