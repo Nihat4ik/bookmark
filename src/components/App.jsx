d05645577b8bd5eba9b12bf37eaa610c17ca8a8f
Nihat4ik <aliyevnihats@outlook.com>
2022-01-22 15:59:50 +0400

    renamed unlogical folder names

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import "./app.scss";
 import Header from "./header/Header";
 import Intro from "./intro/Intro";
-import Features from "./section2/Features";
-import Extensions from "./section3/Extensions";
-import Faq from "./section4/Faq";
-import Contact from "./section5/Contact";
+import Features from "./features/Features";
+import Extensions from "./extensions/Extensions";
+import Faq from "./faq/Faq";
+import Contact from "./contacts/Contact";
 import Footer from "./footer/Footer";
 
 const App = () => {
